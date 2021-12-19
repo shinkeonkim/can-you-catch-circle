f716edcbb151e22b32c9b7d742fe4ea330dc2f80
shinkeonkim <singun11@kookmin.ac.kr>
2021-12-19 18:30:10 +0900

    feat: window size에 따라 animation의 속도가 다른 문제 수정

--- src/pages/JustMoving.vue
+++ src/pages/JustMoving.vue
@@ -5,22 +5,40 @@
   </h1>
 
   <div style="display: flex; justify-content: center;">
-    <Circle @click="moveTo('what-next')" class="move-left-to-right" />
+    <Circle
+      @click="moveTo('what-next')"
+      class="move-left-to-right"
+      :style="`animation-duration: ${compuetedAnimationDuration}s;`"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import {
-  defineComponent, inject,
+  computed,
+  defineComponent, inject, onMounted, ref,
 } from 'vue'
 import Circle from '@/components/Circle.vue'
 
 export default defineComponent({
     name: "JustMovingPage",
     setup() {
+      const VELOCITY = 150;
       const moveTo = inject('moveTo') as Function;
+      const windowWidth = ref(1);
+      const compuetedAnimationDuration = computed(() => windowWidth.value / VELOCITY);
+
+      onMounted(() => {
+        windowWidth.value = window.innerWidth;
+        window.onresize = () => {
+          windowWidth.value = window.innerWidth;
+        }
+      })
+
       return {
         moveTo,
+        windowWidth,
+        compuetedAnimationDuration,
       } 
     },
     components: {
@@ -29,58 +47,43 @@ export default defineComponent({
 })
 </script>
 
-<style>
+
+<style lang="scss">
 
 .move-left-to-right {
-  animation: just-left-to-right 20s linear infinite;
+  animation-name: just-left-to-right;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
   -webkit-animation-name: just-left-to-right;
-  -webkit-animation-duration: 20s;
   -webkit-animation-timing-function:linear;
   -webkit-animation-iteration-count: infinite;
 }
 
 @-webkit-keyframes just-left-to-right {
   0% {
-    -webkit-transform: translateX(0);
-  }
-
-  25% {
-    -webkit-transform: translateX(45vw);
+    -webkit-transform: translateX(-45vw);
   }
 
   50% {
-    -webkit-transform: translateX(0);
+    -webkit-transform: translateX(45vw);
   }
 
-  75% {
-    -webkit-transform: translateX(-45vw);
-  }  
-
   100% {
-    -webkit-transform: translateX(0);
-  }  
+    -webkit-transform: translateX(-45vw);
+  }
 }
 
 @keyframes just-left-to-right {
   0% {
-    transform: translateX(0);
-  }
-
-  25% {
-    transform: translateX(45vw);
+    transform: translateX(-45vw);
   }
 
   50% {
-    transform: translateX(0);
+    transform: translateX(45vw);
   }
 
-  75% {
-    transform: translateX(-45vw);
-  }  
-
   100% {
-    transform: translateX(0);
-  }  
+    transform: translateX(-45vw);
+  }
 }
-
 </style>
